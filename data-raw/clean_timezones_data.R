a409ce590d1be09f33ba9a63b4511cf12a2568c1
Brooke Anderson <geanderson@jhsph.edu>
2016-05-24 10:47:50 -0600

    Clean up timezone data frame

--- data-raw/clean_timezones_data.R
+++ data-raw/clean_timezones_data.R
@@ -3,7 +3,33 @@ library(dplyr)
 
 setwd("data-raw")
 timezones <- readOGR(dsn = "c_05ap16a", layer = "c_05ap16")
-timezones <- timezones@data %>%
-  rename(lat = LAT, lon = LON, state = STATE, fips = FIPS, tz = TIME_ZONE,
+
+clean_tzs <- function(tz){
+  tz <- toupper(tz)
+  tz[grepl("E", tz)] <- "US/Eastern"
+  tz[grepl("C", tz)] <- "US/Central"
+  tz[grepl("M", tz)] <- "US/Mountain"
+  tz[grepl("P", tz)] <- "US/Pacific"
+  tz[grepl("H", tz)] <- "US/Hawaii"
+  tz[grepl("A", tz)] <- "US/Alaska"
+  return(tz)
+}
+
+county_tzs <- timezones@data %>%
+  dplyr::rename(lat = LAT, lon = LON, state = STATE, fips = FIPS, tz = TIME_ZONE,
          county_name = COUNTYNAME) %>%
-  select(-CWA, -FE_AREA)
+  select(-CWA, -FE_AREA) %>%
+  filter(!(state %in% c("AS", "GU", "PR", "VI"))) %>%
+  mutate(tz = clean_tzs(tz),
+         fips = as.character(fips))
+county_tzs$fips[county_tzs$fips == "46102"] <- "46113"
+county_tzs <- rbind(county_tzs,
+                    c(NA, NA, "HI", NA, "15005", "US/Hawaii"),
+                    c(NA, NA, "VA", NA, "51515", "US/Eastern"))
+
+library(choroplethr)
+to_map <- county_tzs %>%
+  select(fips, tz) %>%
+  dplyr::rename(region = fips, value = tz) %>%
+  mutate(region = as.numeric(as.character(region)))
+county_choropleth(unique(to_map))
