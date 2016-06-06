b1b6a6c2c2083bc7904f2ba285f229a31990e8c4
Brooke Anderson <geanderson@jhsph.edu>
2016-06-06 10:20:12 -0600

    Add vignette

--- R/add_local_time.R
+++ R/add_local_time.R
@@ -18,14 +18,20 @@
 #' @examples
 #' library(hurricaneexposure)
 #' data(closest_dist)
+#' library(lubridate)
 #' floyd <- filter(closest_dist, storm_id == "Floyd-1999") %>%
 #'          mutate(closest_date = ymd_hm(closest_date, tz = "UTC"))
 #' floyd <- add_local_time(df = floyd, date_colname = "closest_date")
+#'
+#' @importFrom dplyr %>%
+#'
+#' @export
 add_local_time <- function(df, date_colname){
   df$datetime <- df[ , date_colname]
+  df <- dplyr::mutate_(df, fips = ~ as.numeric(fips))
   df <- dplyr::left_join(df, county_tzs, by = "fips") %>%
     mutate(local_time = mapply(calc_single_datetime, datetime, tz = tz),
-           local_date = lubridate::ymd(substring(local_time, 1, 8)))
+           local_date = substring(local_time, 1, 8))
   return(df)
 }
 
@@ -43,6 +49,8 @@ add_local_time <- function(df, date_colname){
 #' @examples
 #' utc_time <- as.POSIXct("1999-09-15 14:30:00", tz = "GMT")
 #' local_time <- calc_single_datetime(utc_time, tz = "US/Eastern")
+#'
+#' @export
 calc_single_datetime <- function(datetime, tz){
   local_time <- lubridate::with_tz(datetime, tzone = tz)
   local_time <- format(local_time, format = "%Y%m%d%H%M%S")
